f9ca5dcac4e65b7675e4c3ee8dd946d13b860c57
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-11 17:38:57 -0400

    initial commit

---
+++ backend/base/products.py
@@ -0,0 +1,27 @@
+products = [
+    {
+      '_id': '1',
+      'name': 'Airpods Wireless Bluetooth Headphones',
+      'description':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 89.99,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'iPhone 11 Pro 256GB Memory',
+      'description':
+        'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 599.99,
+      'countInStock': 0,
+      'rating': 4.0,
+      'numReviews': 8,
+    }
+]
+
---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+// import logo from './logo.svg';
+// import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect triggered")
+    async function fetchProducts() {
+      const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+  console.log(products)
+
+  // useEffect(() => {
+  //   console.log("useEffect triggered")
+  //   fetch("http://localhost:8000/api/products/")
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+  // }, [])
+  const productsToDisplay = products.map(product => <p>{product.name}</p>)
+
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+      {productsToDisplay}
+    </div>
+  );
+}
+
+export default App;
