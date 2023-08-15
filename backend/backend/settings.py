11ff1311225105a74c918e7a83e5a2e41bc4eb46
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-15 16:36:40 -0400

    create a static file in backend for images and add STATICFILES_DIRS list in settings to point to that file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+# pointing into the root directory and into static folder
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
