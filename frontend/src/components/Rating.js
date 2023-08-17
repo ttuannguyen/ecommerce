4c789615eb61cd1b669967e6fc0a81d7240f509a
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-16 23:55:26 -0400

    add more span tags and styling logic for a 5-star rating system

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -13,6 +13,46 @@ const Rating = ({ value, text, color }) => {
             }>
             </i>
         </span>
+        <span>
+            <i style={{ color }} className={
+                value >= 2 
+                    ? 'fas fa-star'
+                    : value >= 1.5
+                        ? 'fas fa-star-half-alt'
+                        : 'far fa-star'
+            }>
+            </i>
+        </span>
+        <span>
+            <i style={{ color }} className={
+                value >= 3 
+                    ? 'fas fa-star'
+                    : value >= 2.5
+                        ? 'fas fa-star-half-alt'
+                        : 'far fa-star'
+            }>
+            </i>
+        </span>
+        <span>
+            <i style={{ color }} className={
+                value >= 4
+                    ? 'fas fa-star'
+                    : value >= 3.5
+                        ? 'fas fa-star-half-alt'
+                        : 'far fa-star'
+            }>
+            </i>
+        </span>
+        <span>
+            <i style={{ color }} className={
+                value >= 5 
+                    ? 'fas fa-star'
+                    : value >= 4.5
+                        ? 'fas fa-star-half-alt'
+                        : 'far fa-star'
+            }>
+            </i>
+        </span>
     </div>
   )
 }
