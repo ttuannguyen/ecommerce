7c0c23faf25b3ad2c11934efa899f7a071416474
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-16 23:49:20 -0400

    add a Rating component with logic to style a star for rating and import it in Product component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -13,6 +14,15 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </a>
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {/* {product.rating} from {product.numReviews} reviews */}
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
   )
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+        <span>
+            <i style={{ color }} className={
+                value >= 1 
+                    ? 'fas fa-star'
+                    : value >= 0.5
+                        ? 'fas fa-star-half-alt'
+                        : 'far fa-star'
+            }>
+            </i>
+        </span>
+    </div>
+  )
+}
+
+export default Rating
