61cba5052125dccd9f290a2f7e5497d2d2eec172
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-19 16:45:32 -0400

    import LinkContainer from react router bootstrap and wrap LinkContainers around Nav.Links and remove hrefs

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = ({ products }) => {
 
     return (
         <div>
-            <h1>Newly Arrivals!</h1>
+            <h1>New Arrivals!</h1>
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
