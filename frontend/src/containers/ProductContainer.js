71d77f4edeb90569592ffd7d708b326904912096
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-20 19:55:37 -0400

    add a Link element in the Product's show page to navigate back to home

--- frontend/src/containers/ProductContainer.js
+++ frontend/src/containers/ProductContainer.js
@@ -7,14 +7,13 @@ import Rating from '../components/Rating'
 
 const ProductContainer = ({ products }) => {
 
-    //TO DO: Find the product using the product's id in params
     const params = useParams();
     const product = products.find(p => p._id === parseInt(params.id))
-    console.log(product)
   
     return (
     <div>
         {product.name}
+        <Link to='/home' className='btn btn-light my-3'>Back to Products</Link>
     </div>
   )
 }
