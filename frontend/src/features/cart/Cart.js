6c87864501017d769302d2d6d79300910453efca
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-08-30 16:53:41 -0400

    in Cart, add a reduce array method to derive the total price to pay from t he unit price and the quantity

--- frontend/src/features/cart/Cart.js
+++ frontend/src/features/cart/Cart.js
@@ -93,6 +93,7 @@ const Cart = () => {
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+              ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
             </ListGroup.Item>
           </ListGroup>
         </Card>
