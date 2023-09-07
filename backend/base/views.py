862f49a379e7b14c823173b13633f8b7474a86a1
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-09-07 14:38:28 -0400

    add a User serializer and import it in views

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from .products import products
 
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
 
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
