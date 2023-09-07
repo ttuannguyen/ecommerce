1e089dda25ae6e4cf298ef05ee1a6f9eb59bd463
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-09-07 17:59:04 -0400

    import IsAuthenticated from rest frameowork to authenticate a user

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .products import products
 
@@ -69,6 +70,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
