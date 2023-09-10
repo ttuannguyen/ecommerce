7c14233f17a2126a81754c3cb72d0d0e119e8024
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-09-09 22:41:39 -0400

    clean up urls and views files

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .products import products
 
 from django.contrib.auth.models import User
 
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -16,7 +13,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     # @classmethod
@@ -42,35 +38,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-
-
-# @api_view(['GET', 'POST'])
-# def getRoutes(request):
-#     routes = [
-#         '/api/products',
-#         '/api/products/create',
-#         '/api/products/upload',
-#         '/api/products/<id>/reviews',
-#         '/api/products/top',
-#         '/api/products/<id>',
-#         '/api/products/delete/<id>',
-#         '/api/products/<update>/<id>',
-#     ]
-#     return Response(routes)
-
-# def getRoutes(request):
-#     routes = [
-#         '/api/products',
-#         '/api/products/create',
-#         '/api/products/upload',
-#         '/api/products/<id>/reviews',
-#         '/api/products/top',
-#         '/api/products/<id>',
-#         '/api/products/delete/<id>',
-#         '/api/products/<update>/<id>',
-#     ]
-#     return JsonResponse(routes, safe=False)
 
 
 @api_view(['POST'])
@@ -105,25 +72,3 @@ def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data) 
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data) 
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
-
-
-# def getProduct(request, pk):
-#     product = None
-#     for i in products:
-#         if i['_id'] == pk:
-#             product = i
-#             break
-#     return Response(product) 
