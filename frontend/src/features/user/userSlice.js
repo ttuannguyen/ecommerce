62456b7f1dd457ae5276ddb2b39cce1f7bb41644
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-09-10 16:43:31 -0400

    build the login function to log a user in with the email and password inputs in the User slice and add reducers to handle different response statuses

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -3,14 +3,55 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    entities: {}
+    entities: {},
+    errors: null
 }
+
+
+export const login = createAsyncThunk(
+    'user/fetchMe',
+    async (email, password) => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const response = await axios.post(
+            '/api/users/login/',
+            {'username': email, 'password': password},
+            config
+        )
+        return response.data
+    }
+)
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(login.pending, (state) => {
+            state.status = 'loading'
+            state.errors = null // reset errors at page refresh
+        })
+        .addCase(login.fulfilled, (state, action) => {
+            state.status = 'fulfilled'
+            if (action.payload.errors) {
+                state.errors = action.payload.errors
+                // state.loginErrors = action.payload.errors
+            } else {
+                // state.loggedIn = true
+                state.errors = null
+                state.entities = action.payload
+            }
+        })
+        .addCase(login.rejected, (state) => {
+            state.status = 'rejected'
+        })   
     }
+
 })
 
 
